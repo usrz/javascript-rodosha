7f693241f44e00aa6b4cfefc93c740f49f1c5f6f
Pier Fumagalli <ianosh@gmail.com>
2014-11-25 13:06:03 +0900

    Use Esquire plugins for testing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,5 @@
 module.exports = function(grunt) {
 
-  /* Chai for simple mocha */
-  var chai = require('chai');
-  chai.config.includeStack = true;
-  global.expect = chai.expect;
-
   /* Grunt initialization */
   grunt.initConfig({
 
@@ -20,7 +15,7 @@ module.exports = function(grunt) {
     },
 
     /* Simple mocha */
-    simplemocha: {
+    'esquire-mocha': {
       'options': { slow: 500 },
       'default': {
         src: [ 'index.js',
@@ -85,7 +80,7 @@ module.exports = function(grunt) {
 
   /* Load our plugins */
   grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-esquire-mocha');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-jsdoc-ng');
