1d583e0e4d10cb256923aecbf6ee0de489a79526
Pier Fumagalli <ianosh@gmail.com>
2014-11-25 13:42:11 +0900

    Fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     /* Unit testing */
-    karma: {
+    'karma': {
       unit: {
         configFile: 'karma.conf.js',
         runnerPort: 9999,
@@ -26,21 +26,8 @@ module.exports = function(grunt) {
       }
     },
 
-    /* Concatenate task */
-    concat: {
-      build: {
-        src: 'src/**/*.js',
-        dest: 'rodosha.js',
-        options: {
-          banner: '(function() {\n',
-          footer: '\n})();\n',
-          separator: ';\n'
-        }
-      }
-    },
-
     /* Uglify task */
-    uglify: {
+    'uglify': {
       build: {
         src: 'src/**/*.js',
         dest: 'rodosha.min.js',
@@ -84,14 +71,13 @@ module.exports = function(grunt) {
   /* Load our plugins */
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-esquire-mocha');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-jsdoc-ng');
   grunt.loadNpmTasks('grunt-gh-pages');
 
   /* Default tasks */
-  grunt.registerTask('default', ['test', 'concat', 'uglify']);
-  grunt.registerTask('test', ['karma', 'simplemocha']);
+  grunt.registerTask('default', ['test', 'uglify']);
+  grunt.registerTask('test', ['karma', 'esquire-mocha']);
   grunt.registerTask('docs', ['jsdoc-ng', 'gh-pages']);
 
 };
